fcd0073e1a896add3aa77f5a5bebcebd9e2650c2
Arjit Jain <thearjitjain@gmail.com>
2021-04-01 16:54:42 +0530

    Update run_expts.sh

--- run_expts.sh
+++ run_expts.sh
@@ -10,7 +10,7 @@ do
     cat "data/"$DATA"/"$INDICES > "data/"$DATA"/"$INIT_INDICES$INDICES
     for VAR in $(seq 1 10)
         do
-        python $PYFILE $ARGS --indices $INIT_INDICES$INDICES --out $OUTFILE $4
+        python $PYFILE $ARGS --indices $INIT_INDICES$INDICES --out $OUTFILE
         cat "data/"$DATA"/"$INIT_INDICES$INDICES "data/"$DATA"/"$OUTFILE > "data/"$DATA"/"$TEMPFILE
         cat "data/"$DATA"/"$TEMPFILE > "data/"$DATA"/"$INIT_INDICES$INDICES
         done
